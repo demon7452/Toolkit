8cf13c1c6ce10e74ac608aa6fef1736e4cbcb7b9
lixiong <lixiong@guiji.ai>
2019-08-30 17:24:51 +0800

    test where read the config

--- toolkit-web/src/test/java/com/xiong/toolkit/ToolkitWebApplicationTests.java
+++ toolkit-web/src/test/java/com/xiong/toolkit/ToolkitWebApplicationTests.java
@@ -3,6 +3,7 @@ package com.xiong.toolkit;
 import com.xiong.toolkit.api.TestApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,10 +16,13 @@ public class ToolkitWebApplicationTests {
     @Resource
     private TestApi testApi;
 
+    @Value("${config.name}")
+    private String name;
+
     @Test
     public void contextLoads() {
 
-        testApi.test("xiong");
+        testApi.test("xiong" + name);
     }
 
 }
